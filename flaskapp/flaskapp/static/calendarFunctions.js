4eeb05e49a78a555ebb8568c276dc51a685e4f7a
zelataino <gtaino@wustl.edu>
2017-04-16 22:33:26 -0500

    Headers work

--- flaskapp/flaskapp/static/calendarFunctions.js
+++ flaskapp/flaskapp/static/calendarFunctions.js
@@ -1,8 +1,9 @@
 var days_of_week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+var times = ["6:00am", "7:00am", "8:00am", "9:00am", "10:00am", "11:00am", "12:00pm", "1:00pm", "2:00pm", "3:00pm", "4:00pm", "5:00pm", "6:00pm", "7:00pm", "8:00pm", "9:00pm", "10:00pm"];
 
 function setupDates(){
 	var today = new Date();
-	console.log(today);
 }
 
 function getDaysInMonth(month, year){
@@ -10,24 +11,39 @@ function getDaysInMonth(month, year){
 	return new Date(year, month, 0).getDate();
 }
 
-function createHeader(){
+function createHeader(day, month, date){
 	var $headerCell = $("<td>", {"class": "header"});
 	var $day_div = $("<div>");
-	// var $month_div = $("<div>");
-	// var $date_div = $("<div>");
+	var $month_div = $("<div>");
+	var $date_div = $("<div>");
 
-	$day_div.html("YOMAMMA");
-	$("#weekrow").append($day_div);
+	$day_div.html(day);
+	$month_div.html(month);
+	$date_div.html(date);
+
+	$("#weekrow").append($headerCell);
+	$headerCell.append($day_div);
+	$headerCell.append($month_div);
+	$headerCell.append($date_div);
 }
 
 $(document).ready(function(){
 	var today = new Date();
 	var day = today.getDay();
+	var month = today.getMonth();
+	var date = today.getDate();
+
 	var $weekrow = $("<tr>", {id: "weekrow"});
 	$("#time-table").append($weekrow);
-	createHeader();
+
 	for(var i = 0; i<7; i++){
-		console.log(days_of_week[day%7]);
+		var dayText = days_of_week[day%7];
+		createHeader(dayText, months[month], date);
+
 		day = day+1;
+		date = date+1;
 	}
+
+
+
 });
